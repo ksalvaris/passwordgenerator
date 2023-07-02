3b674002a26134a3548806cdb6983f45c1a61865
Pickled_Onion <ksalvaris@gmail.com>
2023-07-02 16:24:33 +0800

    first attempt

---
+++ Develop/script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// define variables for characters that will be called in forloops 
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var special = [" ","#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var upperAlpha = alpha.toLocaleUpperCase();
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
